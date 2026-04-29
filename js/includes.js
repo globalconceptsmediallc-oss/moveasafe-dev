f164eb063843b80f40cb5468dd5ab6040a98c905
kkpayne1-dot <kkpayne1@gmail.com>
2026-04-29 09:51:11 -0400

    Update includes.js

--- js/includes.js
+++ js/includes.js
@@ -1,23 +1,13 @@
 (function () {
-  function getBasePath() {
-    var path = window.location.pathname;
-    var depth = path.split('/').filter(Boolean).length;
-
-    if (path.endsWith('.html')) {
-      depth = depth - 1;
-    }
-
-    if (depth <= 0) return './';
-    return '../'.repeat(depth);
-  }
+  var script = document.currentScript;
+  var scriptSrc = script.getAttribute('src');
+  var basePath = scriptSrc.replace(/js\/includes\.js$/, '');
 
   function loadPartial(id, file) {
     var target = document.getElementById(id);
     if (!target) return;
 
-    var base = getBasePath();
-
-    fetch(base + 'partials/' + file)
+    fetch(basePath + 'partials/' + file)
       .then(function (response) {
         if (!response.ok) throw new Error('Could not load ' + file);
         return response.text();
@@ -26,7 +16,7 @@
         target.innerHTML = html;
 
         target.querySelectorAll('[data-href]').forEach(function (link) {
-          link.setAttribute('href', base + link.getAttribute('data-href'));
+          link.setAttribute('href', basePath + link.getAttribute('data-href'));
         });
       })
       .catch(function (error) {
